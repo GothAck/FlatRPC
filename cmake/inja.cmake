871c7e7ef4f7c056eb4129d89b19f51856e1d005
Greg "GothAck" Miell <greg@gothack.ninja>
2020-02-28 08:18:48 +0000

    Generate embedded inja files in binary build dir

--- cmake/inja.cmake
+++ cmake/inja.cmake
@@ -1,9 +1,13 @@
 function(gen_inja_h)
 foreach(source IN LISTS ARGV)
+
+get_filename_component(sourcedir "${source}" DIRECTORY)
+
 add_custom_command(
   OUTPUT ${source}.h
-  COMMAND xxd -i ${source} > ${source}.h
-  COMMAND touch -r ${source} ${source}.h
+  COMMAND mkdir -p ${CMAKE_CURRENT_BINARY_DIR}/${sourcedir}
+  COMMAND xxd -i ${source} > ${CMAKE_CURRENT_BINARY_DIR}/${source}.h
+  COMMAND touch -r ${source} ${CMAKE_CURRENT_BINARY_DIR}/${source}.h
   WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
   DEPENDS ${source}
 )
