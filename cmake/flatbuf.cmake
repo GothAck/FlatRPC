f44d51f9184b8e9924d85152030f553548d02e57
Greg "GothAck" Miell <greg@gothack.ninja>
2020-03-16 08:30:24 +0000

    Move flatbuf _generated to binary build dirs

--- cmake/flatbuf.cmake
+++ cmake/flatbuf.cmake
@@ -4,11 +4,12 @@ function(build_flatbuf)
         get_filename_component(flatbuf_name "${input}" NAME_WE)
         get_filename_component(flatbuf_dir "${input}" DIRECTORY)
         add_custom_command(
-            OUTPUT ${CMAKE_CURRENT_SOURCE_DIR}/${flatbuf_name}_generated.h
+            OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${flatbuf_name}_generated.h
             COMMAND
                 flatc
                 --cpp --gen-mutable --gen-name-strings --gen-object-api --gen-compare --reflect-names
                 --cpp-std c++17
+                -o ${CMAKE_CURRENT_BINARY_DIR}
                 ${input}
             DEPENDS ${input}
             WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
@@ -16,11 +17,11 @@ function(build_flatbuf)
         )
         add_custom_target(
             gen_${flatbuf_name}_fbs
-            DEPENDS ${CMAKE_CURRENT_SOURCE_DIR}/${flatbuf_name}_generated.h
+            DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${flatbuf_name}_generated.h
         )
-        set_source_files_properties(${CMAKE_CURRENT_SOURCE_DIR}/${flatbuf_name}_generated.h PROPERTIES GENERATED TRUE)
-        add_library(${flatbuf_name}_fbs STATIC ${CMAKE_CURRENT_SOURCE_DIR}/${flatbuf_name}_generated.h)
-        target_include_directories(${flatbuf_name}_fbs INTERFACE ./${flatbuf_dir})
+        set_source_files_properties(${CMAKE_CURRENT_BINARY_DIR}/${flatbuf_name}_generated.h PROPERTIES GENERATED TRUE)
+        add_library(${flatbuf_name}_fbs STATIC ${CMAKE_CURRENT_BINARY_DIR}/${flatbuf_name}_generated.h)
+        target_include_directories(${flatbuf_name}_fbs INTERFACE ${CMAKE_CURRENT_BINARY_DIR})
         set_target_properties(${flatbuf_name}_fbs PROPERTIES LINKER_LANGUAGE CXX)
 #        target_sources(${flatbuf_name}_fbs INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/${flatbuf_name}_generated.h)
     endforeach ()
